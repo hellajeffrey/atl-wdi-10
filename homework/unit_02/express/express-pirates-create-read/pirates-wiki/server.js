e4a45eb01b111193f27fb8dba4e12b67bd1e90a0
hellajeffrey <Jeffreyhenneberg@gmail.com>
2017-06-06 14:41:12 -0400

    test

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,14 @@
-//your code here
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+var pirateController = require('./controllers/pirates.js');
+app.use("/pirates", pirateController);
+
+app.listen(3000, function () {
+  console.log('listening on port 3000!')
+})
+
