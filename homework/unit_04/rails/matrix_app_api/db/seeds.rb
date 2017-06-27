f2a23a0a5f469ff83116bdeb0b1b5f66c95efb8b
hellajeffrey <Jeffreyhenneberg@gmail.com>
2017-06-27 09:33:24 -0400

    catching up

--- homework/unit_04/rails/matrix_app_api/db/seeds.rb
+++ homework/unit_04/rails/matrix_app_api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Character.create([
+
+{ name: "Neo", description: "A computer hacker who becomes 'The One' in leading the war against machines." },
+{ name: "Trinity", description: "Neo's BFF and GF, made famous by cracking a database so secure that she is famous among hackers." },
+{ name: "Morpheus", description: "Pill-carrying captain of the Nebuchadnezzar wanted by 'The Agents.'" },
+{ name: "Agent Smith", description: "A sentient computer program carefully disguised like an average-looking human male who is Neo's archenemy. "},
+
+])
